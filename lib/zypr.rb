77065198ea16df553270f3c3a7caff0ecfad6b95
Clinton Judy <clinton@westarete.com>
2011-11-25 12:29:29 -0500

    WIP.\n\nCompletely rewritten API calls to match Zypr's functionality.\nFixed spec for creating a user.\nJust working on spec for logging in.

--- lib/zypr.rb
+++ lib/zypr.rb
@@ -6,14 +6,35 @@ class Zypr
   @@config = YAML.load(File.read("config.yml"))["ZYPR"]
   base_uri @@config['base_domain']
 
-  def self.open_connection
-    result = self.request("auth/login", :key => @@config['api_key'])
-    result["response"]["information"]["code"] == 200
+  def self.create(username, password, password_confirm)
+    result = self.request("auth/create_user", 
+      :username => username,
+      :password => password,
+      :password_confirm => password_confirm
+    )["response"]["information"]
+    
+    result["code"] == 200 && result["status"] == "OK"
   end
 
-private
+  def initialize(username, password)
+    result = self.request("auth/login", :username => username, :password => password)["response"]["information"]
+    if result["code"] == 200 && result["status"] == "OK"
+      @token = result["token"]
+      return self
+    else
+      raise "Incorrect username and/or password."
+    end
+  end
+
+  def request(path, options={})
+    options[:token] = @token
+    Zypr.request(path, options)
+  end
 
   def self.request(path, options={})
-    get("/api/v#{@@config['version']}/#{path}/", options)
+    if path == "auth/login" || path == "auth/create_user"
+      options[:key] = @@config['app']['api_key']
+    end
+    result = get("/api/v#{@@config['version']}/#{path}/", :query => options)
   end
 end
