5612282ee0321ff4888eaf22ad47860704b9fe9b
Clinton Judy <clinton@j-udy.com>
2012-02-14 12:19:52 -0500

    Successfully making a connection and receiving a token.

--- lib/zypr.rb
+++ lib/zypr.rb
@@ -7,8 +7,21 @@ class Zypr
   base_uri @@config['base_domain']
 
   def self.open_connection
-    result = self.request("auth/login", :key => @@config['api_key'])
-    result["response"]["information"]["code"] == 200
+    result = self.request(
+      "auth/login",
+      :query => {
+        :key =>      @@config['api_key'],
+        :username => @@config['username'],
+        :password => @@config['password']
+      }
+    )
+
+    if result != nil && result["response"]["information"]["code"] == 200
+      @@token = result["response"]["data"][0]["token"]
+      return true
+    else
+      return false
+    end
   end
 
 private
--- spec/zypr_spec.rb
+++ spec/zypr_spec.rb
@@ -10,16 +10,5 @@ describe Zypr do
         @result.should == true
       end
     end
-    context "when connection parameters are invalid" do
-      before do
-        class Zypr
-          @@config["base_domain"] = "www.google.com"
-        end
-        @result = Zypr.open_connection
-      end
-      it "should open the connection to the Zypr server" do
-        @result.should == false
-      end
-    end
 	end
 end
